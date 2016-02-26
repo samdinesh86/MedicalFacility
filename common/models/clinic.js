297e7dabaf4099e39cbd566181bfa9f3e4193eaf
pdinesh <pdinesh@augustasoftsol.com>
2016-02-26 15:48:44 +0530

    Updated the clinic.js for new application logic

--- common/models/clinic.js
+++ common/models/clinic.js
@@ -1,3 +1,13 @@
 module.exports = function (Clinic) {
+  Clinic.greet = function(msg, cb) {
+    cb(null, 'Greetings... ' + msg);
+  }
 
+  Clinic.remoteMethod(
+    'greet',
+    {
+      accepts: {arg: 'msg', type: 'string'},
+      returns: {arg: 'greeting', type: 'string'}
+    }
+  );
 };
