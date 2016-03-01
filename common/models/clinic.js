6d2071fe3b9b0b60062399eeab69f3fc15198a9a
agowtham <agowtham@augustasoftsol.com>
2016-03-01 12:42:50 +0530

    Added Single Module Insert Function

--- common/models/clinic.js
+++ common/models/clinic.js
@@ -1,4 +1,5 @@
 module.exports = function (Clinic) {
+  var loopback = require('loopback');
   Clinic.greet = function(msg, cb) {
     cb(null, 'Greetings... ' + msg);
   }
@@ -10,4 +11,34 @@ module.exports = function (Clinic) {
       returns: {arg: 'greeting', type: 'string'}
     }
   );
+
+ //Post Method Example Using Remote Method
+  Clinic.addclinic = function(data,cb) {
+    debugger;
+       Clinic.on('attached',function(){
+        var override = Clinic.find;
+        var Doctor = Clinic.app.models.Doctor;
+       console.log(Doctor);
+        });
+       Clinic.create({
+        ClinicName:data.ClinicName,
+        Address:data.Address,
+        Phone:data.Phone
+       },function(err){
+        cb(null, err);
+       });
+
+      
+  };
+
+  Clinic.remoteMethod(
+    'addclinic',
+    {
+      accepts: { arg: 'data', type: 'object', http: { source: 'body' } } ,
+      returns: { arg: 'Data', type: 'string'}
+    
+    }
+  );
+
 };
+
